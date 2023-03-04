868c2678510e3acfb7f7d4ceeeb03e1b2caa9d83
Angela Moreno Prado <angelamorenoprado@MacBook-Pro-de-Angela.local>
2023-03-04 13:40:41 +0100

    Create project and first steps

---
+++ vuex-playlist/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <product-list-one :products="products"></product-list-one>
+  <product-list-two :products="products"></product-list-two>
+</template>
+
+<script>
+
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  data() {
+    return {
+      products: [
+        {name: 'Banana Skin', price: 20},
+        {name: 'Shiny Star', price: 40},
+        {name: 'Green Shells', price: 60},
+        {name: 'Red Shells', price: 80}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuex-playlist/src/components/ProductListTwo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id='ProductListTwo'>
+        <h2>Product List Two</h2>
+        <ul>
+            <li v-for="product in products" :key="product.name">
+                <span class='name'>{{ product.name }}</span>
+                <span class='price'>{{ product.price }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductListTwo',
+    props: ['products'],
+    components: {
+
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    li {
+        list-style: none;
+    }
+</style>
