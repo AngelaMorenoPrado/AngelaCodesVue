55b6891c5f239a16d67dece1de7b35db98470930
Angela Moreno Prado <angelamorenoprado@MacBook-Pro-de-Angela.local>
2023-03-04 17:09:30 +0100

    New

---
+++ vuex/src/components/ApplicationVue.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="ApplicationVue">
+    <h1>First change</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ApplicationVue',
+  setup() {
+
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
